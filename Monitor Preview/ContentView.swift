619dba4362399cbce1316e4002e04b9ff3a05710
Zhang Maiyun <me@maiyun.me>
2022-04-15 08:37:08 -0700

    Initial

---
+++ Monitor Preview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Monitor Preview
+//
+//  Created by Zhang Maiyun on 2022-04-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Monitor Preview/Monitor_PreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monitor_PreviewApp.swift
+//  Monitor Preview
+//
+//  Created by Zhang Maiyun on 2022-04-15.
+//
+
+import SwiftUI
+
+@main
+struct Monitor_PreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
