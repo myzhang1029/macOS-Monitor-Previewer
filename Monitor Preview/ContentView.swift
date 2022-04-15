1b1d5c86262fe218a114eefb0cf9212654daa2d1
Zhang Maiyun <me@maiyun.me>
2022-04-15 14:38:11 -0700

    Finish v1

--- Monitor Preview/ContentView.swift
+++ Monitor Preview/ContentView.swift
@@ -7,10 +7,53 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var displayList: [CGDirectDisplayID] = []
+    @State private var error: Optional<String> = .none
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Spacer()
+                Text("Display Selection").font(.title2)
+                Spacer()
+                List {
+                    /* the display id is unique */
+                    ForEach($displayList, id: \.self) {
+                        let dId = $0.wrappedValue;
+                        NavigationLink(
+                            destination: DisplayPreviewView(displayId: dId),
+                            label: {
+                                VStack {
+                                    HStack {
+                                        Text(displayName(displayId: dId))
+                                        Spacer()
+                                    }
+                                    HStack {
+                                        Text("\(displayProp(displayId: dId)) @ \(dId)")
+                                        Spacer()
+                                    }
+                                }
+                            })
+                    }
+                }
+                Spacer()
+                Button("Refresh Displays", action: refreshDisplays)
+                Spacer()
+            }
+        }
+        .onAppear(perform: refreshDisplays)
+    }
+    
+    /** Refresh list of displays */
+    func refreshDisplays() {
+        switch getDisplays() {
+        case .success(let displays):
+            displayList = displays
+        case .failure(let cgerror):
+            error = .some("CoreGraphics error \(cgerror)")
+        }
     }
 }
 
