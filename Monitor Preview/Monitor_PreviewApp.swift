552090a4b5fbf533274de5c20aa12e235f9253e2
Zhang Maiyun <me@maiyun.me>
2026-06-04 23:02:32 -0400

    Add AppDelegate to quit automatically

--- Monitor Preview/Monitor_PreviewApp.swift
+++ Monitor Preview/Monitor_PreviewApp.swift
@@ -5,13 +5,24 @@
 //  Created by Zhang Maiyun on 2022-04-15.
 //
 
+import AppKit
+import Foundation
 import SwiftUI
 
 @main
 struct Monitor_PreviewApp: App {
+    // Based on https://stackoverflow.com/a/65743682/9347959
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
